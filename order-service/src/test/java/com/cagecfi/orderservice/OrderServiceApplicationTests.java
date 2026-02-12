11cf48acd489ff77c1dd376472d4defd942b7d42
simon-oks <simon-oks@outlook.fr>
2026-02-12 15:43:26 +0000

    Création du projet et des différents services

---
+++ order-service/src/test/java/com/cagecfi/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cagecfi.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
